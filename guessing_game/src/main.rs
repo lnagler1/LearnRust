d372d52860d4697d6cb11977491bfc2c78083334
lnagler1 <lnagler1@htl-steyr.ac.at>
2023-03-27 01:59:12 +0200

    Checking if Input is number, and comparing guessed_number with secret_number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,14 @@
-use std::io;
+use std::{io, cmp::Ordering};
+use rand::Rng;
 
 fn main() {
-     let error_message = "Failed to read line";
+    let error_message = "Failed to read line";
 
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number}");
 
     println!("Please input your guess.");
 
@@ -13,5 +18,13 @@ fn main() {
         .read_line(&mut guess)
         .expect(error_message);
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
