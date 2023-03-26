6ff414ebe1516b96228362b83fa0bd91619d7b57
lnagler1 <lnagler1@htl-steyr.ac.at>
2023-03-26 22:10:20 +0200

    Take in user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+     let error_message = "Failed to read line";
+
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect(error_message);
+
+    println!("You guessed: {guess}");
 }
